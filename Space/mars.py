4dc0e5862ab3bea460b593320e6b47665bda0a6e
Pierre Siddall <43399998+Pierre-siddall@users.noreply.github.com>
2023-02-17 15:48:01 +0000

    Changed code to the right file

--- Space/mars.py
+++ Space/mars.py
@@ -1,6 +1,14 @@
 import requests
 
+def get_date():
+    pass
+def _read_key():
+    with open("Key.rtf", "r") as f:
+        api_key = f.readlines()
+    return api_key[-1][0:-1]
+
 
 
 if __name__ == "__main__":
-    pass
+    key = _read_key()
+    print(key)
